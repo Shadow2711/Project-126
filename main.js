3ccc6d56e52fac9f7f3eda339c7319bfd1268da4
Shadow2711 <83654982+Shadow2711@users.noreply.github.com>
2022-08-09 17:00:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+song1 = "";
+song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+leftWristScore = 0;
+songStatus = "";
+function preload(){
+    song1 = loadSound("Sunflower.mp3");
+    song2 = loadSound("Believer.mp3");
+}
+function setup(){
+    canvas = createCanvas(600 , 500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    posNet = ml5.poseNet(video , modelLoaded);
+    poseNet.on('pose' , gotPoses);
+}
+function modelLoaded(){
+    console.log("model is loaded");
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+    }
+}
+function draw(){
+    image(video , 0 , 0 , 600 , 500);
+}
